38647242195942c29f90c23e1946389ad53db619
Makoto Tanabe <xekid78@outlook.com>
2018-02-24 20:29:58 +0900

    Add files via upload

---
+++ typeconv.rb
@@ -0,0 +1,17 @@
+puts "** ケース1 **"
+num1 = 3.15
+puts num1
+puts num1.to_i
+puts ""
+
+puts "** ケース２ **"
+num2 = 5
+puts num2
+puts num2.to_f
+puts ""
+
+puts "** ケース３ **"
+text = "123"
+puts text
+puts text.to_i * 100
+puts text.to_f
